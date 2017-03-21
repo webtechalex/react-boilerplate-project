516cf47281d5bbc6fe72ea0b3346da1151b297dc
Alex Brown <alexanderbrown.info@gmail.com>
2017-03-21 20:58:32 +0100

    added test environment

---
+++ test/app.spec.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import { expect } from 'chai';
+
+import App from '../app/components/App';
+
+describe('first test', function () {
+  it('should run a test', function() {
+  	expect(documentRef).to.equal(document);
+  });
+});
